89930f8273ec9230ac201b1d7481282b6b2c501d
ruizdiazmartin <ruizdiazmartin@gmail.com>
2020-11-04 11:15:38 -0300

    Se agrega funcionalidad paginado con botones anterior y suguiente.

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -22,9 +22,10 @@ class Buscador extends Component {
 					<div className="form-group col-md-8">
 						<input ref={this.busquedaRef} type="text" className="form-control form-control-lg" placeholder="Busca tu imagen. Ejemplo: Futbol" />
 					</div>
-					<div className="form-group col-md-4">
+					<div className="form-group col-md-4" >
 						<input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar..." />
 					</div>
+
 				</div>
 			</form>
 		)
